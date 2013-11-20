7c2c8b02b5f519ce132618448c2112dacc1f6c93
4n4kin <nilesh5760@gmail.com>
2013-11-20 12:10:06 +0530

    Cleaning Code

--- Code/0.Cleaning.R
+++ Code/0.Cleaning.R
@@ -1,26 +1,21 @@
 #Text Cleaning: tm Code
-
-library(tm)
-text1$text->text
-sample<- Corpus(VectorSource(text),readerControl=list(language="english"))
-tm_map(sample, function(x) iconv(enc2utf8(x), sub = "byte"))
-sample<-tm_map(sample,tolower)
-sample<-tm_map(sample,removePunctuation)
-sample <- tm_map(sample, stripWhitespace)
-sample<-tm_map(sample,removeNumbers)
-sample<-tm_map(sample,removeWords,stopwords('smart'))
-sample <- tm_map(sample, stripWhitespace)
-#inspect(sample[1:3])
-
-#tm_map(sample,stemDocument)
-sample <- tm_map(sample, stripWhitespace)
-#inspect(sample[1:3])
-dtm <- DocumentTermMatrix(sample)
-#inspect(sample[1:3])
-
-print("Preparing for lda")
-library('lda')
-sample->q
-lexicalize(q)->a
-a.documents<-a$documents
-a.vocab<-a$vocab
+clean<-function(text){
+  library(tm)
+  text1$text->text
+  sample<- Corpus(VectorSource(text),readerControl=list(language="english"))
+  tm_map(sample, function(x) iconv(enc2utf8(x), sub = "byte"))
+  sample<-tm_map(sample,tolower)
+  sample<-tm_map(sample,removePunctuation)
+  sample <- tm_map(sample, stripWhitespace)
+  sample<-tm_map(sample,removeNumbers)
+  sample<-tm_map(sample,removeWords,stopwords('smart'))
+  sample <- tm_map(sample, stripWhitespace)
+  #inspect(sample[1:3])
+  
+  #tm_map(sample,stemDocument)
+  sample <- tm_map(sample, stripWhitespace)
+  #inspect(sample[1:3])
+  dtm <- DocumentTermMatrix(sample)
+  #inspect(sample[1:3])
+  return(list(sample,dtm))
+}
